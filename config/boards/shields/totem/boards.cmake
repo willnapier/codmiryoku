99b7e5b95548741f21d500ea5d6606fc517759ad
Will Napier <will@willnapier.com>
2025-05-18 15:17:47 +0100

    Fix boards.cmake to register custom Totem shields correctly for ZMK

--- config/boards/shields/totem/boards.cmake
+++ config/boards/shields/totem/boards.cmake
@@ -1,6 +1,7 @@
-zephyr_library()
+# config/boards/shields/totem/boards.cmake
 
-board_runner_args(seeeduino_xiao_ble "--shield=totem_left")
-board_runner_args(seeeduino_xiao_ble "--shield=totem_right")
+# Define the list of available shields
+set(SHIELD_LIST totem totem_left totem_right)
 
+# Register this directory for ZMK to search for shields
 add_board_directory(${CMAKE_CURRENT_LIST_DIR}/..)
