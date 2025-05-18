288d57df770808703c550d0ae2f4dd09f031c6b5
Will Napier <will@willnapier.com>
2025-05-18 15:24:58 +0100

    Fix boards.cmake and west.yml to register custom shields

--- config/boards/shields/totem/boards.cmake
+++ config/boards/shields/totem/boards.cmake
@@ -1,7 +1,9 @@
 # config/boards/shields/totem/boards.cmake
 
-# Define the list of available shields
-set(SHIELD_LIST totem totem_left totem_right)
+set(SHIELD_LIST
+  totem
+  totem_left
+  totem_right
+)
 
-# Register this directory for ZMK to search for shields
 add_board_directory(${CMAKE_CURRENT_LIST_DIR}/..)
