da6d61ec94c1511b8862070a221598bcdb5c92ca
Will Napier <will@willnapier.com>
2025-05-18 15:05:36 +0100

    modified boards.cmake, totem.dtsi, totem.overlay, totem_left.overlay, totem_right.overlay

--- config/boards/shields/totem/boards.cmake
+++ config/boards/shields/totem/boards.cmake
@@ -1,2 +1,6 @@
-# config/boards/shields/totem/boards.cmake
-add_board_directory_ifdef(CONFIG_SHIELD_TOTEM config/boards/shields/totem)
+zephyr_library()
+
+board_runner_args(seeeduino_xiao_ble "--shield=totem_left")
+board_runner_args(seeeduino_xiao_ble "--shield=totem_right")
+
+add_board_directory(${CMAKE_CURRENT_LIST_DIR}/..)
