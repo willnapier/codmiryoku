0b4485f8699fc51b9e8e0cec8e758e705bf8a622
Will Napier <will@willnapier.com>
2025-06-03 22:40:18 +0100

    Initial standard ZMK configuration

--- config/boards/shields/totem_right/boards.cmake
+++
@@ -1,6 +0,0 @@
-zephyr_library()
-
-board_runner_args(seeeduino_xiao_ble "--shield=totem_left")
-board_runner_args(seeeduino_xiao_ble "--shield=totem_right")
-
-add_board_directory(${CMAKE_CURRENT_LIST_DIR}/..)
